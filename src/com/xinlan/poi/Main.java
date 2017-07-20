a8b1e235214aa238996d124a7ddd73d132cc46b8
yixin.panyi <panyi@yixin.im>
2017-07-20 13:37:46 +0800

    xls文件读写练习

--- src/com/xinlan/poi/Main.java
+++ src/com/xinlan/poi/Main.java
@@ -1,8 +1,11 @@
 package com.xinlan.poi;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -14,9 +17,42 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			write();
+			
+			readXLSXFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void readXLSXFile() throws IOException {
+		InputStream ExcelFileToRead = new FileInputStream("D:/潘易.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(ExcelFileToRead);
+
+		XSSFSheet sheet = wb.getSheet("附加");
+		
+		//wb.getSheet(arg0)
+		
+		XSSFRow row;
+		XSSFCell cell;
+
+		Iterator rows = sheet.rowIterator();
+		while (rows.hasNext()) {
+			row = (XSSFRow) rows.next();
+			Iterator cells = row.cellIterator();
+			while (cells.hasNext()) {
+				cell = (XSSFCell) cells.next();
+
+				if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING) {
+					System.out.print(cell.getStringCellValue() + " ");
+				} else if (cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC) {
+					System.out.print(cell.getNumericCellValue() + " ");
+				} else {
+					// U Can Handel Boolean, Formula, Errors
+				}
+			}
+			System.out.println();
+		}
+
 	}
 
 	public static void write() throws IOException {
@@ -36,7 +72,7 @@ public class Main {
 				cell.setCellValue("Cell " + r + " " + c);
 			}
 		}
-		
+
 		XSSFSheet sheet2 = wb.createSheet("附加");
 
 		// iterating r number of rows
@@ -45,10 +81,10 @@ public class Main {
 			// iterating c number of columns
 			for (int c = 0; c < 5; c++) {
 				XSSFCell cell = row.createCell(c);
-				cell.setCellValue("海军——"+c);
+				cell.setCellValue("海军——" + c);
 			}
 		}
-		
+
 		FileOutputStream fileOut = new FileOutputStream(excelFileName);
 		// write this workbook to an Outputstream.
 		wb.write(fileOut);
